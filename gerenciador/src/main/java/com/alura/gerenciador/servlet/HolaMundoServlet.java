6d211f831e66ba6a1f8f8de88a070362908ef8e6
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2022-01-05 22:30:34 -0300

    Video 1.4

---
+++ gerenciador/src/main/java/com/alura/gerenciador/servlet/HolaMundoServlet.java
@@ -0,0 +1,30 @@
+package com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//hola
+@WebServlet(urlPatterns = "/hola")
+public class HolaMundoServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out =resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hola Mundo! Felicitaciones por crear tu primer Servlet!");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("Se ejecutó el servlet");
+		
+	}
+
+}
