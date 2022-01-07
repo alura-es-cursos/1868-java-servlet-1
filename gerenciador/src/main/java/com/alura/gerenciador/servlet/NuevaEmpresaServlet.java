a6e433ea1abfca72673ef3aec46b9c0329ad7d28
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2022-01-07 20:37:04 -0300

    Video 2.1

---
+++ gerenciador/src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
@@ -0,0 +1,24 @@
+package com.alura.gerenciador.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class NuevaEmpresaServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("nueva empresa registrada");
+		String nombreEmpresa = request.getParameter("nombre");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Empresa " + nombreEmpresa + " registrada!</body></html>");
+	}
+
+}
