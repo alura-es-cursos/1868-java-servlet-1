e641f90a2b4066aef6ffbacab11e6599e1561196
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2022-01-08 15:10:44 -0300

    Aula 2.2 y 2.3

--- gerenciador/src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
+++ gerenciador/src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
@@ -13,10 +13,12 @@ public class NuevaEmpresaServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		System.out.println("nueva empresa registrada");
+		
 		String nombreEmpresa = request.getParameter("nombre");
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa " + nombreEmpresa + " registrada!</body></html>");
 	}
